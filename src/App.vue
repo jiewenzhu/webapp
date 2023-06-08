72ad6eb0155690b314e3254c8b7c7e66a949b416
zhujiewen <948226125@qq.com>
2023-06-08 14:27:44 +0800

    feat:first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,25 @@
+<!--
+ * @Date: 2023-06-08 14:21:18
+ * @LastEditors: zhujiewen 948226125@qq.com
+ * @LastEditTime: 2023-06-08 14:22:31
+ * @FilePath: /webapp/src/App.vue
+-->
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
